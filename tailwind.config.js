caadee14335df97040572fd4ce687d6082f6190a
Yiyi Sun <yiyisun@gmail.com>
2022-12-20 12:04:45 -0500

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.css", "./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
