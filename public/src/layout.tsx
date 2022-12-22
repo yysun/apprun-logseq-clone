4ff3a5552d047cd9465fa8d2a8016ccab60ecfe4
Yiyi Sun <yiyisun@gmail.com>
2022-12-21 20:03:50 -0500

    style using tailwindcss

--- public/src/layout.tsx
+++ public/src/layout.tsx
@@ -1,30 +1,29 @@
 import app from 'apprun';
 
 const toggle_left_panel = () => {
-  document.getElementById('left-panel').classList.toggle('open');
+  document.getElementById('left-panel').classList.toggle('hidden');
 }
 
-export default () => <div class="vh-100">
-  <main class="main">
-    <div class="container-fliud d-flex p-2 fs-4" id="toolbar">
-      <a class="ms-2" onclick={toggle_left_panel}><i class="px-1">
+export default () => <>
+  <div class="toolbar w-full p-3 flex flex-row" id="toolbar">
+    <a class="toolbar-button mx-1" onclick={toggle_left_panel}>
+      <i class="">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="6" x2="20" y2="6"></line><line x1="4" y1="12" x2="20" y2="12"></line><line x1="4" y1="18" x2="20" y2="18"></line></svg></i></a>
-      <a class="ms-2"><i class="px-1">
+    <a class="mx-1">
+      <i class="">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="10" cy="10" r="7"></circle><line x1="21" y1="21" x2="15" y2="15"></line></svg>
       </i></a>
-      <a class="ms-auto ms-4"><i class="ti ti-brand-tabler"></i></a>
-      <a class="ms-2"><i class=""></i>
-        <span class="ui__icon ti ls-icon-dots "><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="5" cy="12" r="1"></circle><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle></svg></span>
-      </a>
-    </div>
+    <a class="mx-1"><i class=""></i>
+      <span class=""><svg xmlns="http://www.w3.org/2000/svg" class="i" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="5" cy="12" r="1"></circle><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle></svg></span>
+    </a>
+  </div>
 
-    <div class="container-fliud d-flex">
-      <div class="vh-100" id="left-panel">
-        <aside class="px-4">Keywords</aside>
-      </div>
-      <div class="vh-100 flex-grow-1 overflow-scroll" id="main-panel">
-        <div class="px-4" id='my-app'></div>
-      </div>
+  <div class="flex">
+    <div class="w-64 max-h-screen p-4 hidden" id="left-panel">
+      <aside class="">Keywords</aside>
     </div>
-  </main>
-</div>;
+    <div class="flex-1 max-h-screen overflow-scroll p-4" id="main-panel">
+      <div class="max-w-2xl mx-auto" id='my-app'></div>
+    </div>
+  </div>
+</>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.css", "./src/**/*.tsx"],
+  content: ["./public/src/**/*.{html,css,tsx}"],
   theme: {
     extend: {},
   },
