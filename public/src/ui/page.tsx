012ca7d32b0c802468438104ce62a3ac46d8acb0
Yiyi Sun <yiyisun@gmail.com>
2022-12-24 10:58:14 -0500

    add page component

---
+++ public/src/ui/page.tsx
@@ -0,0 +1,45 @@
+import {app, Component, safeHTML} from 'apprun';
+import { to_html } from '../md';
+import { data } from '../data';
+
+const toggle = el => {
+  el.style.display = el.style.display === 'none' ? 'block' : 'none';
+}
+
+const toggle_block_list = e => {
+  const target = e.target.closest('.block').querySelector('.block-list');
+  if (target) {
+    toggle(target);
+    e.target.classList.toggle('collapsed');
+  }
+}
+
+const create_content = content => {
+  content = to_html(content);
+  return safeHTML(content)[0];
+}
+export default class Page extends Component {
+
+  view = ({ page }) => {
+
+    let { id, children } = page;
+    const block = data.blocks.find(b => b.id === id);
+    let list = children?.map(child => <Page page={child} />);
+    let content = block.content;
+    content = block.type === 'page' ? `<h1>${content}</h1>` : content;
+
+    content = create_content(content);
+
+    return <div class={`block`}
+      id={block.id}>
+      <div class="block-header">
+        <div class="block-bullet">
+          <div class="bullet" onclick={toggle_block_list}></div>
+        </div>
+        <div class="block-content" block={block}>{content}</div>
+      </div>
+      {list && <div class="block-list">{list}</div>}
+    </div>;
+
+  }
+}
