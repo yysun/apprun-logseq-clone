68b97b3cdbeccf419f57edf54d7a2ccf5ec3ccbf
Yiyi Sun <yiyisun@gmail.com>
2022-12-24 16:38:56 -0500

    add page in model

--- public/src/model/page.ts
+++ public/src/model/page.ts
@@ -1,1 +1,35 @@
 import { data } from '../store';
+
+const find_page = block => {
+  const page_block = data.blocks.find(b => b.type === 'page' && b.page === block.page);
+  const page = data.pages.find(p => p.id === page_block.id);
+  return page;
+}
+
+const create_content = page => {
+  const { id, children } = page;
+  const block = data.blocks.find(b => b.id === id);
+  let list = children?.map(child => create_content(child))
+    .filter(c => !!c)
+    .join('\n');
+
+  let content = block.type === 'page' ? '' : block.content;
+
+  if (content && list) {
+    content = content + '\n' + list;
+  } else if (!content) {
+    content = list;
+  } 
+  content = ' '.repeat(block.level) + content;
+  return content;
+};
+
+export const get_page = (block) => {
+  const page = find_page(block);
+  if (!page) return {}
+  const content = create_content(page);
+  return {
+    file_name: `${block.page}.md`,
+    content
+  }
+}
--- public/src/ui/page.tsx
+++ public/src/ui/page.tsx
@@ -18,38 +18,28 @@ const create_content = content => {
   content = to_html(content);
   return safeHTML(content)[0];
 }
-export default class Page extends Component {
 
-  // @on('@refresh-page')
-  // refresh = ({ page }, new_page) => {
-  //   if (page.id === new_page.id) {
-  //     return { page: new_page };
-  //   }
-  // }
+export default function Page({ page }) {
 
-  view = ({ page }) => {
+  let { id, children } = page;
+  const block = data.blocks.find(b => b.id === id);
+  let list = children?.map(child => <Page page={child} />);
+  let content = block.content;
+  // content = block.type === 'page' ? `<h1>${content}</h1>` : content;
 
-    let { id, children } = page;
-    const block = data.blocks.find(b => b.id === id);
-    let list = children?.map(child => <Page page={child} />);
-    let content = block.content;
-    // content = block.type === 'page' ? `<h1>${content}</h1>` : content;
+  content = create_content(content);
 
-    content = create_content(content);
-
-    return <div class={`block`}
-      id={block.id}>
-      <div class="block-header">
-        <div class="block-bullet">
-          <div class="bullet" onclick={toggle_block_list}></div>
-        </div>
-        <div class="block-content" contenteditable="true"
-          $onfocus='@edit-block-begin'
-          $onblur='@edit-block-end'
-          block={block}>{content}</div>
+  return <div class={`block`}
+    id={block.id}>
+    <div class="block-header">
+      <div class="block-bullet">
+        <div class="bullet" onclick={toggle_block_list}></div>
       </div>
-      {list && <div class="block-list">{list}</div>}
-    </div>;
-
-  }
+      <div class="block-content" contenteditable="true"
+        $onfocus='@edit-block-begin'
+        $onblur='@edit-block-end'
+        block={block}>{content}</div>
+    </div>
+    {list && <div class="block-list">{list}</div>}
+  </div>;
 }
