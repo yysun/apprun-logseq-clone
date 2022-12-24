17d4520f6bbd9b0d92731c0d6956ec84385f2563
Yiyi Sun <yiyisun@gmail.com>
2022-12-24 10:58:14 -0500

    add page component

--- public/src/home.tsx
+++ public/src/home.tsx
@@ -1,65 +1,7 @@
-import { app, Component, safeHTML, on } from 'apprun';
-import { to_html }  from './md';
+import { app, Component, on } from 'apprun';
 import { data, select_file } from './data';
-import  search  from './search';
-
-const highlight = (value, indeics) => {
-  let result = '', match = '';
-  let last = 0;
-  for (const [start, end] of indeics) {
-    result += value.substring(last, start);
-    last = end + 1;
-    match = value.substring(start, last);
-    result += (end - start > 0) ? `<mark>${match}</mark>` : match;
-  }
-  result += value.substring(last);
-  return result;
-}
-
-const toggle = el => {
-  el.style.display = el.style.display === 'none' ? 'block' : 'none';
-}
-
-const toggle_block_list = e => {
-  const target = e.target.closest('.block').querySelector('.block-list');
-  if (target) {
-    toggle(target);
-    e.target.classList.toggle('collapsed');
-  }
-}
-
-const create_content = content => {
-  content = to_html(content);
-  return safeHTML(content)[0];
-}
-
-const create_block = (node, hits) => {
-
-  let { id, children } = node;
-  const block = data.blocks.find(b => b.id === id);
-  let list = children?.map(child => create_block(child, hits));
-
-  const hit = !hits || hits.find(b => b.id === id);
-  node.show = hit ? true : children?.some(child => child.show);
-
-  if (!node.show) return null;
-
-  let content = block.content;
-  content = hit?.matches ? highlight(content, hit.matches[0].indices) : content;
-  content = block.type === 'page' ? `<h1>${content}</h1>` : content;
-  content = create_content(content);
-
-  return <div class={`block`}
-    id={block.id}>
-    <div class="block-header">
-      <div class="block-bullet">
-        <div class="bullet" onclick={toggle_block_list}></div>
-      </div>
-      <div class="block-content" block={block}>{content}</div>
-    </div>
-    {list && <div class="block-list">{list}</div>}
-  </div>;
-}
+import search from './search';
+import Page from './ui/page';
 
 const getFile = async () => {
   await select_file();
@@ -79,13 +21,12 @@ export default class extends Component {
 
   view = state => {
     const pages = state.pages || [];
-    const hits = state.hits;
-    const total = hits ? hits.length : data?.blocks.length;
+    const total = data?.blocks.length;
 
     return pages.length > 0 ?
       <div class="page">
         <h1>All Pages ({total})</h1>
-        {pages.map(page => create_block(page, hits))}
+        {pages.map(page => <Page page={page} />)}
       </div> :
       <button $onclick={getFile}>Open...</button>
   }
--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,12 +1,10 @@
 import app from 'apprun';
 import Home from './home';
 import Layout from './ui/layout';
-import './ui-events';
 import './data';
 
 // import load_plugins from './plugins';
 // await load_plugins();
-
 
 new Layout().start(document.body);
 new Home().start('my-app');
