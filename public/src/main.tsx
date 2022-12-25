eed3a77688f7463f93ff9ae7ebfaad031d514053
Yiyi Sun <yiyisun@gmail.com>
2022-12-25 14:08:13 -0500

    redo layout

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -3,11 +3,12 @@ import Home from './home';
 import Layout from './ui/layout';
 import plugins from './plugins';
 import store from './store';
+import app from 'apprun';
 
 await plugins();
 await store();
 
-new Layout().start(document.body);
+app.render(document.getElementById('root'), <Layout />);
 new Home().start('my-app');
 
 
--- public/src/ui/layout.tsx
+++ public/src/ui/layout.tsx
@@ -1,4 +1,5 @@
 import { app, Component } from 'apprun';
+import ToolBar from './top-toolbar';
 import resizable from './resizable';
 import Calander from './calander';
 
@@ -6,52 +7,25 @@ const toggle_left_panel = () => {
   document.getElementById('left-panel').classList.toggle('hidden');
 }
 
-export default class extends Component {
+export default () => <>
+  <div id="left-panel" class="h-screen flex-1">
+    <div id="left-tool-bar" class="h-10 w-full sticky z-10">
+      <ToolBar />
+    </div>
+    <div id="left-main" class="flex-1 flex flex-row pl-64">
+      <div id="left-drawer" class="absolute top-0 left-0 w-64 h-screen overflow-scroll bg-gray-100">
 
-  view = () => <div class="flex flex-col max-h-screen overflow-hidden">
-    <div class="toolbar w-full p-3 flex flex-row" id="toolbar">
-      <a class="toolbar-button mx-1" onclick={toggle_left_panel}>
-        <i class="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="6" x2="20" y2="6"></line><line x1="4" y1="12" x2="20" y2="12"></line><line x1="4" y1="18" x2="20" y2="18"></line></svg></i></a>
-      <a class="mx-1">
-        <i class="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="10" cy="10" r="7"></circle><line x1="21" y1="21" x2="15" y2="15"></line></svg>
-        </i></a>
-      <a class="mx-1"><i class=""></i>
-        <span class=""><svg xmlns="http://www.w3.org/2000/svg" class="i" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="5" cy="12" r="1"></circle><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle></svg></span>
-      </a>
-    </div>
-    <div class="flex overflow-hidden">
-      <div class="w-12 bg-gray-300" id="left-icons">
       </div>
-      <div class="w-64 p-4 min-w-max hidden bg-gray-100" id="left-panel">
-        <Calander />
-      </div>
-      {/* <div class="resizer bg-white" data-direction='horizontal'></div> */}
-      <div class="overflow-scroll w-full p-4" id="main-panel">
-        <div id='my-app'></div>
-      </div>
-      <div class="resizer bg-gray-200" data-direction='horizontal'></div>
-      <div id="right-panel" class="flex-1 flex flex-col">
-        <div id="right-panel-top" class="h-full overflow-scroll"></div>
-        <div class="resizer bg-gray-200" data-direction='vertical'></div>
-        <div id="right-panel-bottom" class="flex-1 overflow-scroll"></div>
+      <div id="left-content" class="flex-1 overflow-scroll">
+        <div id="my-app"></div>
       </div>
     </div>
-    <div id="floating" class="absolute left-0 top-0 max-w-max hidden">
-      Tooltip
-      <div id="arrow" class="absolute"></div>
-    </div>
-  </div>;
+  </div>
+  <div class="resizer bg-gray-200 w-1 h-screen" data-direction='horizontal' ref={e => resizable(e)}></div>
+  <div id="right-panel" class="w-1 h-screen"></div>
 
-  rendered = () => {
-    const w = window.innerWidth - 4 -
-      48 - // document.getElementById('left-icons').getBoundingClientRect().width -
-      //208 -  //document.getElementById('left-panel').getBoundingClientRect().width;
-      0 // document.getElementById('right-panel').getBoundingClientRect().width;
-    document.getElementById('main-panel').style.width = `${w}px`;
-    document.querySelectorAll('.resizer').forEach(function (ele) {
-      resizable(ele);
-    });
-  }
-}
+  <div id="floating" class="absolute left-0 top-0 max-w-max hidden">
+    Tooltip
+    <div id="arrow" class="absolute"></div>
+  </div>
+</>
