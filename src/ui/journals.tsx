97632b15babb60d977d32db2ee93b3fa8e491366
Yiyi Sun <yiyisun@gmail.com>
2023-01-02 18:51:27 -0500

    use data-fns

--- src/ui/journals.tsx
+++ src/ui/journals.tsx
@@ -1,4 +1,5 @@
 import { app, Component, on } from 'apprun';
+import {format } from 'date-fns';
 import { data, dirHandle, select_dir, grant_access, new_page } from '../store';
 import { editor_keydown } from './editor';
 import Page from './page';
@@ -8,8 +9,7 @@ export default class extends Component {
   @on('#journals')
   show = () => {
     let pages = data.pages?.filter(p => p.name.startsWith('journals/')) || [];
-    const date = new Date();
-    const today = `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`;
+    const today = format(new Date(), 'yyyy_MM_dd');
     if (!pages.some(p => p.name === `journals/${today}`)) {
       new_page(`journals/${today}`, '- ');
     }
